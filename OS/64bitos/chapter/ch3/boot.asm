ac7dadd6deb2d66ea07fe95048107b588eb06488
evanshao <shao_rx@outlook.com>
2022-12-25 22:25:07 +0800

    boot.asm

--- OS/64bitos/chapter/ch3/boot.asm
+++ OS/64bitos/chapter/ch3/boot.asm
@@ -4,6 +4,126 @@
 ;当然这里也可以通过DS/ES设置为7c0h实现可以不带org
 org 0x7c00   
 BaseOfStack equ 0x7c00 ;BaseOfStack 标识符 等价于 0x7c00  equ指令 让左边等于右边 但是不会为标识符分配空间 标识符无法重名 无法重新定义。
+BaseOfLoader equ 0x1000
+OffsetOfLoader equ 0x00
+
+RootDirSectors equ 14        ; 目录区占14个扇区
+SectorNumOfRootDirStart equ 19;1+9+9 = 19   ;这里参考LBA和CHS的区别 LBA线性描述 扇区从0开始CHS1开始 因为同密度导致扇区数不一致[柱面就是磁道 内向外 越来越大]
+SectorNumOfFAT1Start equ 1  ; FAT1开始扇区号
+;平衡目录和数据区初始簇号的插值 这里一个扇区就是一个簇 然后标准里数据区起始簇号是2 所以减了2  
+;数据区地址=根目录起始扇区号+根目录所占扇区数+值-2 -2放到前面就是17
+SectorBalance equ 17  
+
+jmp short Label_Start
+nop
+BS_OEMName db 'MINEboot'
+BPB_BytesPerSec dw 512
+BPB_SecPerClus db 1
+BPB_RevdSecCnt db 1
+BPB_NumFATs db 2
+BPB_RootEntCnt dw 224
+BPB_TotSec16 dw 2880
+BPB_Media db  0xf0
+BPB_FATSz16 dw 9
+; 每个磁道的扇区数
+BPB_SecPerTrk dw 18
+BPB_NumHeads dw 2
+BPB_hiddSec dd 0
+BPB_TotSec32 dd 0
+BS_DrvNum db 0
+BS_Reserved1 db 0
+BS_BootSig db 29h
+BS_VolID dd 0
+BS_VolLab db 'boot loader'
+BS_FileSysType db 'FAT12'
+
+;====== read one sector from floppy
+
+Func_ReadOneSector:
+push bp
+mov bp,sp
+; 上面复制了sp 这边-2 相当于预留了两个byte 空间 用以下面的mov byte bp默认ss
+sub esp,2
+mov byte [bp-2],cl ;备份cl 调用方传入的读取的扇区数量
+push bx
+mov bl,[BPB_SecPerTrk]
+;被除数在ax 商在al 余数在ah 这里ax需要调用方放入
+div bl
+inc ah ;余数+1 得到chs的扇区号 
+mov cl,ah ; 13h中断起始扇区号存储在CL
+mov dh,al ; 商放到dh
+shr al,1 ;右移一位得到柱面号(商/BPB_NumHeads)
+mov ch,al ; 获得柱面号 13h中断要求放在CH
+and dh,1 ;得到磁头号 
+pop bx  ;恢复bx
+mov dl,[BS_DrvNum];保存驱动器号
+Label_Go_On_Reading:
+mov ah,2
+mov al,byte [bp-2];之前的cl保存到al 读取al个扇区 
+int 13h
+;jc = jump if Carry 读取错误则CF=1 那就继续跳转到Label_Go_On_Reading
+jc Label_Go_On_Reading
+add esp,2
+pop bp
+ret
+
+;====== search loader.bin
+mov word [SectorNo],SectorNumOfRootDirStart
+
+
+Label_Search_In_Root_Dir_Begin:
+    cmp word [RootDirSizeForLoop],0
+    jz Label_No_LoaderBin
+    dec word [RootDirSizeForLoop]
+    mov ax,00h
+    mov es,ax
+    mov bx,8000h
+    mov ax,[SectorNo]
+    mov cl,1
+    call Func_ReadOneSector
+    mov si,LoaderFileName
+    mov di,8000h
+    cld
+    mov dx,10h
+Label_Search_For_LoaderBin:
+    cmp dx,0
+    jz Label_Goto_Next_Sector_In_Root_Dir
+    dec dx
+    mov cx,11
+Label_Cmp_FileName:
+    cmp cx,0
+    jz Label_FileName_Found
+    dec cx
+    lodsb
+    cmp al,byte [es:di]
+    jz Label_Go_On
+    jmp Label_Different
+Label_Go_On:
+    inc di
+    jmp Label_Cmp_FileName
+Label_Different:
+    and di,0ffe0h
+    add di,20h
+    mov si,LoaderFileName
+    jmp Label_Search_For_LoaderBin
+Label_Goto_Next_Sector_In_Root_Dir:
+    add word [SectorNo],1
+    jmp Label_Search_In_Root_Dir_Begin
+
+;====== display on screen:ERROR:No LOADER Found
+Label_No_LoaderBin:
+    mov ax,1301h
+    mov bx,008ch
+    mov dx,0100h
+    mov cx,21
+    push ax
+    mov ax,ds
+    mov es,ax
+    pop ax
+    mov bp,NoLoaderMessage
+    int 10h
+    jmp $
+
 
 label_Start:
     mov ax,cs
