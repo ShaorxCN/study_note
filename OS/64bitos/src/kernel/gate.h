e8a11094ee4f9d1f625101fe6583afa4fa30b641
evanshao <shao_rx@outlook.com>
2023-05-17 20:56:17 +0800

    fix: apu tss设置失败

--- OS/64bitos/src/kernel/gate.h
+++ OS/64bitos/src/kernel/gate.h
@@ -104,17 +104,17 @@ static inline void set_tss_descriptor(unsigned int n, void *addr)
 void set_tss64(unsigned int *Table, unsigned long rsp0, unsigned long rsp1, unsigned long rsp2, unsigned long ist1, unsigned long ist2, unsigned long ist3,
 			   unsigned long ist4, unsigned long ist5, unsigned long ist6, unsigned long ist7)
 {
-	*(unsigned long *)(TSS64_Table + 1) = rsp0;
-	*(unsigned long *)(TSS64_Table + 3) = rsp1;
-	*(unsigned long *)(TSS64_Table + 5) = rsp2;
+	*(unsigned long *)(Table + 1) = rsp0;
+	*(unsigned long *)(Table + 3) = rsp1;
+	*(unsigned long *)(Table + 5) = rsp2;
 
-	*(unsigned long *)(TSS64_Table + 9) = ist1;
-	*(unsigned long *)(TSS64_Table + 11) = ist2;
-	*(unsigned long *)(TSS64_Table + 13) = ist3;
-	*(unsigned long *)(TSS64_Table + 15) = ist4;
-	*(unsigned long *)(TSS64_Table + 17) = ist5;
-	*(unsigned long *)(TSS64_Table + 19) = ist6;
-	*(unsigned long *)(TSS64_Table + 21) = ist7;
+	*(unsigned long *)(Table + 9) = ist1;
+	*(unsigned long *)(Table + 11) = ist2;
+	*(unsigned long *)(Table + 13) = ist3;
+	*(unsigned long *)(Table + 15) = ist4;
+	*(unsigned long *)(Table + 17) = ist5;
+	*(unsigned long *)(Table + 19) = ist6;
+	*(unsigned long *)(Table + 21) = ist7;
 }
 
 #endif
